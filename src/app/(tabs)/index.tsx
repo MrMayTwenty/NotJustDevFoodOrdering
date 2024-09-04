7f15a40f7fa95c13b51bf5184a34531b1fe659de
MrMayTwenty <agafrancisDroid@gmail.com>
2024-09-04 22:30:00 +0800

    first ui component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,42 +2,20 @@ import { StyleSheet, Image, Text, View } from 'react-native';
 
 import Colors from '@/src/constants/Colors';
 import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
 const product = products[0];
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} resizeMode='contain' />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price.toFixed(2)}</Text>
+    <View>
+      <ProductListItem product={product} />
+      <ProductListItem product={product} />
+      <ProductListItem product={product} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-    overflow: 'hidden',
-  },
-
-  image: {
-    width: '100%',
-    aspectRatio: '1',
-    alignSelf: 'center',
-  },
-
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-    marginTop: 'auto',
-  }
+ 
 
 });
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import { Product } from '../types'
+import Colors from '@/src/constants/Colors'
+type ProductListItemProps = {
+    product: Product,
+}
+
+const ProductListItem = ({product} : ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} resizeMode='contain' />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>{product.price.toFixed(2)}</Text>
+    </View>
+  )
+}
+
+export default ProductListItem
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 20,
+        overflow: 'hidden',
+        marginVertical: 10,
+      },
+    
+      image: {
+        width: '100%',
+        aspectRatio: '1',
+        alignSelf: 'center',
+      },
+    
+      title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginVertical: 10,
+      },
+    
+      price: {
+        color: Colors.light.tint,
+        fontWeight: 'bold',
+        marginTop: 'auto',
+      }
+
+})
